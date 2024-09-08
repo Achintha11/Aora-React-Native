c4edc0cc143895a209e2a4af5477cac9d7c128b1
Achintha <achinthaperis64@gmail.com>
2024-09-09 03:14:22 +0530

    Home screen updated

--- frontend/components/Trending.tsx
+++ frontend/components/Trending.tsx
@@ -1,17 +1,90 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import React, { useState } from "react";
+import { Image } from "react-native";
+import { icons } from "@/constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.8,
+  },
+
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+
+  1: {
+    scale: 0.8,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem.id === item.id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center "
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      console.log(viewableItems);
+      setActiveItem(viewableItems[0].item);
+    }
+  };
   return (
     <FlatList
       data={posts}
-      keyExtractor={(item) => item.id.toString()}
+      keyExtractor={(item) => item.title}
       renderItem={({ item }) => (
-        <View className="w-64 h-64 bg-orange-400 m-2 items-center justify-center">
-          <Text className="text-3xl">Item {item.id}</Text>
-        </View>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
       horizontal
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{ x: 170 }}
     />
   );
 };
